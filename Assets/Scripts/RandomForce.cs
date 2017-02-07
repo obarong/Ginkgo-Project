4e8b831ff12beec891ccf3e254aa53df43c9232d
LF9T72SUZC67A2A\Administrator <dustinljr@gmail.com>
2017-02-07 16:38:21 +0800

    start

---
+++ Assets/Scripts/RandomForce.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = Random.onUnitSphere * 100;
+	}
+	
+	
+}
